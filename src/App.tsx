6caadeed938099641a2e7d85483f063b20075cb5
shreydd <47713047+shreydd@users.noreply.github.com>
2025-04-09 16:26:41 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-4">
+      <Button variant={"default"}>hehere</Button>
+    </div>
+  );
+}
+
+export default App;
